f5a2040ba67538063a88fb595de6a867aa11becd
Teethew <thiagjack@gmail.com>
2022-08-26 02:09:50 -0300

    single responsibility principle

---
+++ src/srp/avoid/Avoid.java
@@ -0,0 +1,51 @@
+package srp.avoid;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Avoid {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        PrintStream console = System.out;
+
+        Journal journal = new Journal();
+        journal.addEntry("Today I'm going to the mall.");
+        journal.addEntry("I bought a bright new pair of shoes!");
+
+        console.println(journal);
+
+        // doesn't make sense
+        journal.save("./journal.txt");
+    }
+}
+
+class Journal {
+    private final List<String> entries = new ArrayList<>();
+    private static int count = 0;
+
+    public void addEntry(String text) {
+        entries.add(++count + ": " + text);
+    }
+
+    public void removeEntry(int index) {
+        entries.remove(index);
+    }
+
+    @Override
+    public String toString() {
+        return String.join(System.lineSeparator(), entries);
+    }
+
+    // clear violation of SRP, persistence shouldn't be a concern of a Journal
+    public void save(String filename) throws FileNotFoundException {
+        try(PrintStream fileStream = new PrintStream(filename)) {
+            fileStream.println(this);
+        }
+    }
+
+    public void load(URI uri) {}
+    public void load(String filename) {}
+}
---
+++ src/srp/encourage/Encourage.java
@@ -0,0 +1,68 @@
+package srp.encourage;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Encourage {
+    public static void main(String[] args) throws IOException {
+        PrintStream console = System.out;
+
+        Journal journal = new Journal();
+        journal.addEntry("Today I'm going to the mall.");
+        journal.addEntry("I bought a bright new pair of shoes!");
+
+        console.println(journal);
+
+        Persistence.saveToFile(journal, "journal.txt");
+
+        console.println(Persistence.load("journal.txt"));
+    }
+}
+
+class Persistence {
+    public static void saveToFile(Journal journal, String filename) throws IOException {
+        if (new File(filename).createNewFile()) {
+            try (PrintStream fileStream = new PrintStream(filename)) {
+                fileStream.println(journal);
+            }
+        }
+    }
+
+    public static Journal load(String filename) throws IOException {
+        Journal journal = new Journal();
+
+        try (Stream<String> lines = Files.lines(Paths.get(filename), StandardCharsets.UTF_8)) {
+            lines.map(line -> line.split(": ")[1])
+                    .forEach(journal::addEntry);
+        }
+
+        return journal;
+    }
+}
+
+class Journal {
+
+
+    private final List<String> entries = new ArrayList<>();
+    private static int count = 0;
+
+    public void addEntry(String text) {
+        entries.add(++count + ": " + text);
+    }
+
+    public void removeEntry(int index) {
+        entries.remove(index);
+    }
+
+    @Override
+    public String toString() {
+        return String.join(System.lineSeparator(), entries);
+    }
+}
